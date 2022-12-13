355adfa0459124f885b4c7124d93c6ad71df8531
MCI Learner <115865871+MCILearner@users.noreply.github.com>
2022-12-13 17:39:34 +0500

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	
+	cout<<"Please enter value one";
+	cin>>valueOne;
+	
+	cout<<"Please enter value two";
+	cin<<valueTwo;
+	
+	cout<<valueOne+valueTwo;
+
+}
+//Coded by Hammad Maqbool
