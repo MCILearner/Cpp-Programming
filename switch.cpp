f666c2461ccb4b61168c0fe0ebef0e5ae1dde0a5
HammadMaqbool <hammad.mqbl@gmail.com>
2022-12-13 18:04:10 +0500

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+		switch()
+		{
+			case 1:
+				//body
+				break;
+			case 2:
+				//body
+				break;
+			default:
+				//body
+		}
+
+}
+//Coded by Hammad Maqbool
